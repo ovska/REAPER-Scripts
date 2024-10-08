47992c3adb65460687effa7fe65dd88664fec94c
ovska <ovaskasi@gmail.com>
2024-10-08 17:23:24 +0300

    add container and "Human MIDI" support for "freeze to kontakt"

--- freeze_to_kontakt.lua
+++ freeze_to_kontakt.lua
@@ -1,5 +1,82 @@
+---@diagnostic disable: redundant-parameter
 function IsInstrument(name)
-    return name:match('^VST3i: ') ~= nil or name:match('^VSTi: ') ~= nil
+    return (name:match("^VST3i: ") ~= nil or name:match("^VSTi: ") ~= nil) and name:match("^VST3i: Human") == nil
+end
+
+function IterateFx(track)
+    local fx = 0
+    local total_container_fx = 0
+    local count = reaper.TrackFX_GetCount(track)
+
+    return function()
+        if fx < count + total_container_fx then
+            local _, container_fx_count_str = reaper.TrackFX_GetNamedConfigParm(track, fx, "container_count")
+            local container_fx_count = tonumber(container_fx_count_str) or 0
+
+            if container_fx_count > 1 then
+                total_container_fx = total_container_fx + container_fx_count - 1
+            end
+
+            local current = fx
+            fx = fx + 1
+            return current
+        end
+
+        return nil
+    end
+end
+
+function Log(msg, ...)
+    if false then
+        local m = string.format(msg, ...)
+        reaper.ShowConsoleMsg(m .. "\n")
+    end
+end
+
+function Is_Instrument(track, fx)
+    local _, name = reaper.TrackFX_GetFXName(track, fx, "")
+    local is_offline = reaper.TrackFX_GetOffline(track, fx)
+
+    local is_online_instrument = (not is_offline) and IsInstrument(name)
+
+    if is_online_instrument then
+        Log("%s - %s %i is instrument", is_online_instrument, name, fx)
+    end
+
+    return is_online_instrument
+end
+
+function Should_Freeze(track, fx)
+    local _, name = reaper.TrackFX_GetFXName(track, fx, "")
+
+    local is_offline = reaper.TrackFX_GetOffline(track, fx)
+
+    -- offline, can't be eof
+    if is_offline then
+        Log("%s - %s %i is offline", false, name, fx)
+        return false
+    end
+
+    if name:match(" LoCut") then
+        Log("%s - %s %i is Lo Cut", true, name, fx)
+        return true
+    end
+
+    if name:match("^JS: Stereo Channel") then
+        Log("%s - %s %i is stereo swap", true, name, fx)
+        return true
+    end
+
+    local _, container_fx_count_str = reaper.TrackFX_GetNamedConfigParm(track, fx, "container_count")
+
+    -- containers right after instruments should be frozen
+    if (tonumber(container_fx_count_str) or 0) ~= 0 then
+        Log("%s - %s %i is Container", true, name, fx)
+        return true
+    end
+
+    Log("%s - %s %i should not be frozen", false, name, fx)
+    return false
 end
 
 -- Returns an array containing FX offline states, or nil if the track shouldn't be rendered
@@ -7,34 +84,35 @@ function PrepareForFreeze(track)
     local fx_count = reaper.TrackFX_GetCount(track)
 
     -- No FX at all, don't freeze
-    if fx_count == 0 then return nil end
+    if fx_count == 0 then
+        return nil
+    end
 
     local fxArray = {} --  the offline state of the FX
-    local instFx = -1  -- instrument FX's index
+    local instFx = -1 -- instrument FX's index
 
-    -- TODO: investigate if reaper.TrackFX_GetInstrument() can do the same thing
-
-    for fx = 0, fx_count - 1 do
-        local _, name = reaper.TrackFX_GetFXName(track, fx, '')
-        local is_offline = reaper.TrackFX_GetOffline(track, fx)
-
-        -- Instrument already found, record the current offline state of the effect
+    for fx in IterateFx(track) do
         if instFx ~= -1 then
-            table.insert(fxArray, is_offline)
-        end
-
-        -- Check if there exists a non-offline instrument FX
-        if IsInstrument(name) and not is_offline then
-            if instFx ~= -1 then
-                -- Multiple instruments, TODO: error message
-                return nil
+            -- already found an instrument, see if we should freeze after
+            if #fxArray == 0 and Should_Freeze(track, fx) then
+                -- is container or low cut
+                instFx = fx + 1
+            else
+                -- should freeze
+                table.insert(fxArray, reaper.TrackFX_GetOffline(track, fx))
             end
+        elseif Is_Instrument(track, fx) then
             instFx = fx + 1
         end
     end
 
     -- instrument not found, or it's offline
-    if instFx == -1 then return nil end
+    if instFx == -1 then
+        return nil
+    end
+
+    -- local _, name = reaper.GetTrackName(track)
+    -- Log('%s freezing at index %s', name, instFx)
 
     for index = 0, #fxArray - 1 do
         reaper.TrackFX_SetOffline(track, index + instFx, true)
@@ -53,7 +131,9 @@ end
 local selected_count = reaper.CountSelectedTracks2(0, false)
 
 -- No tracks selected
-if selected_count == 0 then return end
+if selected_count == 0 then
+    return
+end
 
 local trackArr = {} -- hold selected tracks in an array as might need to render only a subset
 
@@ -74,11 +154,11 @@ for i = 1, selected_count do
     if restore == nil then
         reaper.SetTrackSelected(track, false) -- unselect track if it doesn't have instruments
     else
-        table.insert(trackRestoreArr, { track, restore })
+        table.insert(trackRestoreArr, {track, restore})
     end
 end
 
-reaper.Main_OnCommand(41223, 0) -- Freeze selected tracsk to stereo stems
+reaper.Main_OnCommand(41223, 0) -- Freeze selected tracks to stereo stems
 
 for i = 1, #trackRestoreArr do
     RestoreFx(trackRestoreArr[i][1], trackRestoreArr[i][2])
